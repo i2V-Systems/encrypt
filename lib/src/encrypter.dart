e556250627310c4321de17adf4860f6d43e85b24
Ishita-i2v <ishita.singh@i2vsys.com>
2024-12-20 16:13:45 +0530

    excrypt added local changes

--- lib/src/encrypter.dart
+++ lib/src/encrypter.dart
@@ -3,70 +3,44 @@ part of encrypt;
 /// Wraps Algorithms in a unique Container.
 class Encrypter {
   final Algorithm algo;
+  final iv = IV.fromUtf8("weaxO23UXGedmzfT");
 
   Encrypter(this.algo);
 
   /// Calls [encrypt] on the wrapped Algorithm using a raw binary.
-  Encrypted encryptBytes(List<int> input, {IV? iv, Uint8List? associatedData}) {
+  Encrypted encryptBytes(List<int> input, [IV? iv]) {
     if (input is Uint8List) {
-      return algo.encrypt(input, iv: iv, associatedData: associatedData);
+      if (iv != null) {
+        return algo.encrypt(input, iv: iv);
+      }
+      return algo.encrypt(input, iv: this.iv);
     }
 
-    return algo.encrypt(
-      Uint8List.fromList(input),
-      iv: iv,
-      associatedData: associatedData,
-    );
+    return algo.encrypt(Uint8List.fromList(input), iv: this.iv);
   }
 
   /// Calls [encrypt] on the wrapped Algorithm.
-  Encrypted encrypt(
-    String input, {
-    IV? iv,
-    Uint8List? associatedData,
-  }) {
-    return encryptBytes(
-      convert.utf8.encode(input),
-      iv: iv,
-      associatedData: associatedData,
-    );
+  Encrypted encrypt(String input, [IV? iv]) {
+    return encryptBytes(convert.utf8.encode(input));
   }
 
   /// Calls [decrypt] on the wrapped Algorith without UTF-8 decoding.
-  List<int> decryptBytes(Encrypted encrypted,
-      {IV? iv, Uint8List? associatedData}) {
-    return algo
-        .decrypt(encrypted, iv: iv, associatedData: associatedData)
-        .toList();
+  List<int> decryptBytes(Encrypted encrypted) {
+    return algo.decrypt(encrypted, iv: this.iv).toList();
   }
 
   /// Calls [decrypt] on the wrapped Algorithm.
-  String decrypt(
-    Encrypted encrypted, {
-    IV? iv,
-    Uint8List? associatedData,
-  }) {
-    return convert.utf8.decode(
-      decryptBytes(encrypted, iv: iv, associatedData: associatedData),
-      allowMalformed: true,
-    );
+  String decrypt(Encrypted encrypted) {
+    return convert.utf8.decode(decryptBytes(encrypted), allowMalformed: true);
   }
 
   /// Sugar for `decrypt(Encrypted.fromBase16(encoded))`.
-  String decrypt16(String encoded, {IV? iv, Uint8List? associatedData}) {
-    return decrypt(
-      Encrypted.fromBase16(encoded),
-      iv: iv,
-      associatedData: associatedData,
-    );
+  String decrypt16(String encoded) {
+    return decrypt(Encrypted.fromBase16(encoded));
   }
 
   /// Sugar for `decrypt(Encrypted.fromBase64(encoded))`.
-  String decrypt64(String encoded, {IV? iv, Uint8List? associatedData}) {
-    return decrypt(
-      Encrypted.fromBase64(encoded),
-      iv: iv,
-      associatedData: associatedData,
-    );
+  String decrypt64(String encoded) {
+    return decrypt(Encrypted.fromBase64(encoded));
   }
 }
